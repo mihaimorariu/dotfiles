53c5bd4124107c629c6822e34eff992b41766f8f
Mihai Morariu <mihaimorariu@gmail.com>
2018-03-21 18:13:57 +0900

    Include [pkg_name]/include[pkg_name] only for current file.

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -3,21 +3,24 @@ from glob import glob
 
 base = os.path.dirname(os.path.abspath(__file__))
 
+# Get package name from the 'include' directory. Convention is:
+# [path_to_pkg]/[pkg_name]/include.
+def getPackageNameFromIncludeDir(directory):
+	return os.path.basename(os.path.dirname(directory))
+
+# Search for 'include' directories, recursively, starting from 'base_directory'.
+# Ignore the ones whose names begin with one of the given prefixes.
 def findIncludeDirsRecursively(base_directory):
 	prefixes     = ('__', '.')
 	include_dirs = []
 
 	for root, names, _ in os.walk(base_directory):
 		names[:]      = [n for n in names if not n.startswith(prefixes)]
-		current_dirs  = [os.path.join(root, n) for n in names if n == 'include']
-
-		pkg_name      = os.path.basename(root)
-		current_dirs += [os.path.join(d, pkg_name) for d in current_dirs if os.path.exists(os.path.join(d, pkg_name))]
-
-		include_dirs += current_dirs
+		include_dirs += [os.path.join(root, n) for n in names if n == 'include']
 
 	return include_dirs
 
+# Define flags for the current file.
 def FlagsForFile(filename, **kwargs):
 	filedir = os.path.dirname(filename)
 	flags = [
@@ -36,8 +39,16 @@ def FlagsForFile(filename, **kwargs):
 		'-I', '.',
 	]
 
-	for path in findIncludeDirsRecursively(base):
-		flags += ['-I', path]
+	# Include all [pkg_name]/include directories.
+	include_dirs = findIncludeDirsRecursively(base)
+	for path in include_dirs: flags += ['-I', path]
+
+	# For the current file include the [pkg_name]/include/[pkg_name] directory.
+	for path in include_dirs:
+		if filename.startswith(os.path.dirname(path)):
+			pkg_name  = getPackageNameFromIncludeDir(path)
+			flags    += ['-I', os.path.join(path, pkg_name)]
+			break
 
 	return {
 		'flags': flags,
