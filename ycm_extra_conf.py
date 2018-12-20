97296a073369dc8656a2e9f0b92eb354592b2f4f
Mihai Morariu <mihaimorariu@gmail.com>
2018-12-20 13:33:32 +0900

    Check if directories exist (in ycm_extra_conf.py) before including them.

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -1,57 +1,64 @@
 import os
 from glob import glob
 
-base = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Get package name from the 'include' directory. Convention is:
 # [path_to_pkg]/[pkg_name]/include.
-def getPackageNameFromIncludeDir(directory):
-	return os.path.basename(os.path.dirname(directory))
+def get_package_name(base_dir):
+    return os.path.basename(os.path.dirname(base_dir))
 
-# Search for 'include' directories, recursively, starting from 'base_directory'.
+# Search for 'include' directories, recursively, starting from 'base_dir'.
 # Ignore the ones whose names begin with one of the given prefixes.
-def findIncludeDirsRecursively(base_directory):
-	prefixes     = ('__', '.')
-	include_dirs = []
-
-	for root, names, _ in os.walk(base_directory):
-		names[:]      = [n for n in names if not n.startswith(prefixes)]
-		include_dirs += [os.path.join(root, n) for n in names if n == 'include']
-
-	return include_dirs
+def find_include_dirs(base_dir):
+    prefixes = ('__', '.')
+    dirs = []
+    for root, names, _ in os.walk(base_dir):
+        names[:] = [n for n in names if not n.startswith(prefixes)]
+        dirs += [os.path.join(root, n) for n in names if n == 'include']
+    return dirs
 
 # Define flags for the current file.
 def FlagsForFile(filename, **kwargs):
-	filedir = os.path.dirname(filename)
-	flags = [
-		'-x', 'c++',
-		'-Wall',
-		'-Wextra',
-		'-Wpedantic',
-		'-fPIC',
-		'-std=c++11',
-		'-isystem', '/usr/include/boost',
-		'-isystem', '/usr/include/eigen3',
-		'-isystem', '/usr/include/opencv4',
-		'-isystem', '/usr/include/opencv',
-		'-isystem', '/usr/include/pcl-1.8',
-		'-isystem', '/usr/include/vtk',
-		'-isystem', '/opt/ros/melodic/include',
-		'-I', '.',
-	]
+    flags = [
+        '-x', 'c++',
+        '-Wall',
+        '-Wextra',
+        '-Wpedantic',
+        '-fPIC',
+        '-std=c++11',
+        '-I', '.',
+    ]
 
-	# Include all [pkg_name]/include directories.
-	include_dirs = findIncludeDirsRecursively(base)
-	for path in include_dirs: flags += ['-I', path]
+    # Define the system include directories.
+    include_dirs = [
+        '/usr/include/boost',
+        '/usr/include/eigen3',
+        '/usr/include/opencv4',
+        '/usr/include/opencv',
+        '/usr/include/pcl-1.8',
+        '/usr/include/vtk',
+        '/opt/ros/melodic/include',
+        '/opt/ros/melodic/include/xmlrpcpp/',
+    ]
 
-	# For the current file include the [pkg_name]/include/[pkg_name] directory.
-	for path in include_dirs:
-		if filename.startswith(os.path.dirname(path)):
-			pkg_name  = getPackageNameFromIncludeDir(path)
-			flags    += ['-I', os.path.join(path, pkg_name)]
-			break
+    # Check if the directories exist and include them.
+    for path in include_dirs:
+        if os.path.exists(path):
+            flags += ['-isystem', path]
 
-	return {
-		'flags': flags,
-		'do_cache': True
-	}
+    # Include all [pkg_name]/include directories.
+    include_dirs = find_include_dirs(current_dir)
+    for path in include_dirs: flags += ['-I', path]
+
+    # For the current file include the [pkg_name]/include/[pkg_name] directory.
+    for path in include_dirs:
+        if filename.startswith(os.path.dirname(path)):
+            pkg_name = get_package_name(path)
+            flags += ['-I', os.path.join(path, pkg_name)]
+            break
+
+    return {
+        'flags': flags,
+        'do_cache': True
+    }
