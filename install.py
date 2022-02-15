7d452bab39d46447e351c88f8d17da3bd88ac81e
Mihai Morariu <mihaimorariu@gmail.com>
2022-02-15 18:54:36 +0200

    Add terminfo for alacritty and xterm.

--- install.py
+++ install.py
@@ -27,15 +27,19 @@ def main():
     backup_dir = os.path.join(BAK_DIR, timestamp)
     os.makedirs(backup_dir)
 
-    mappings = [('gitconfig', '.gitconfig'), ('i3', '.i3'),
-                ('alacritty', '.config/alacritty'), ('tmux.conf', '.tmux.conf'),
-                ('nvim/init.vim', '.config/nvim/init.vim'),
-                ('nvim/autoload', '.config/nvim/autoload'),
-                ('nvim/colors', '.config/nvim/colors'),
-                ('xinitrc', '.xinitrc'), ('Xresources', '.Xresources'),
-                ('i3/bumblebee_status/bumblebee-status.conf',
-                 '.config/bumblebee-status.conf'),
-                ('zshrc', '.zshrc')]
+    mappings = [
+        ('gitconfig', '.gitconfig'),
+        ('i3', '.i3'),
+        ('alacritty', '.config/alacritty'),
+        ('tmux.conf', '.tmux.conf'),
+        ('nvim/init.vim', '.config/nvim/init.vim'),
+        ('nvim/autoload', '.config/nvim/autoload'),
+        ('xinitrc', '.xinitrc'), ('Xresources', '.Xresources'),
+        ('i3/bumblebee_status/bumblebee-status.conf', '.config/bumblebee-status.conf'),
+        ('zshrc', '.zshrc'),
+        ('alacritty.ti', '.alacritty.ti'),
+        ('xterm.ti', '.xterm.ti')
+    ]
 
     for src, dst in mappings:
         ln(src, dst, backup_dir)
