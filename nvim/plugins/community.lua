fb43a4bf207355bac75f9ebc129fa49558c6b6b2
Mihai Morariu <mihaimorariu@gmail.com>
2023-12-16 08:47:50 +0200

    Add more nvim plugins.

--- nvim/plugins/community.lua
+++ nvim/plugins/community.lua
@@ -4,6 +4,17 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.programming-language-support.csv-vim" },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
